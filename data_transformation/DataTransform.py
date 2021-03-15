40540d1349aca6491d21da2c5e411dd15df34f11
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-03-14 20:12:27 -0400

    Created first draft of graph functions

---
+++ data_transformation/DataTransform.py
@@ -0,0 +1,13 @@
+"""
+Author: Saif Gorges
+Date: March 11 2021
+"""
+import pandas as pd
+
+def function(comments_dict):
+    # Check if error. Exit function if not none.
+    if comment_data.get('error'):
+        return
+
+    # This line will get the data
+    comment_data = comments_dict.get('data')
