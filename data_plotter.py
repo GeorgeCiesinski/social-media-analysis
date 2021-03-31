a63d3fdb892555c5d8a2e7ba6c6b107a409af725
George Ciesinski <george.ciesin@gmail.com>
2021-03-31 19:59:58 -0400

    Completed data_plotter. Pending saif's changes to make final modifications and test.

--- data_plotter.py
+++ data_plotter.py
@@ -1,40 +1,58 @@
 from data_transformation.DataTransform import DataTransform
+from entities.util.database_manager import DatabaseManager
 from logs.Logger import base_logger
 
 logger = base_logger.getChild(__name__)
 
 
+def plot_all_graphs(data_transform, submission_id):
+	"""
+	Receives a submission id and calls DataTransform functions to plot the data into graphs.
+
+	:param DataTransform data_transform: Instance of DataTransform class used to plot graphs
+	:param str submission_id: Submission.id for the submission
+	"""
+
+	# Create submission dict
+	submission_dict = {
+		'id': submission_id
+	}
+
+	# Populate submission_dict with information from database
+	DatabaseManager.extract_submission(submission_dict)
+
+	# Extract comments_dict with comments and sentiment for submission
+	comments_dict = DatabaseManager.extract_comments_sentiment(submission_dict)
+
+	# Plot Graphs
+	data_transform.overall_sentiment_replies(comments_dict)
+	data_transform.overall_sentiment_upvotes(comments_dict)
+	data_transform.sentiment_pie(comments_dict)
+	data_transform.sentiment_timeline(comments_dict)
+
 
 '''
-INSTANTIATE (TEMP)
+INSTANTIATE CLASSES
 '''
 
-# Instantiate DataTransform
-df = DataTransform()
+dt = DataTransform()
 
 '''
-GRAPHS
+Read Job List
 '''
-
-df.overall_sentiment_replies(comments_dict)
-df.overall_sentiment_upvotes(comments_dict)
-df.sentiment_pie(comments_dict)
-df.sentiment_timeline(comments_dict)
-
-
-
 
 job_list_directory = 'plot_job_list.txt'
 
 logger.info(f'Reading job/{job_list_directory}.')
 
-# Read file and plot each url
+# Read file and extract submission ids
 with open(job_list_directory, "r") as plot_job_list:
-	urls = plot_job_list.readlines()
+	submission_ids = plot_job_list.readlines()
 
-logger.info(f'Found {len(urls)} jobs.')
+logger.info(f'Found {len(submission_ids)} jobs.')
 
-for url in urls:
-
-	logger.info(f'Scraping url: {url}')
-	scrape_submission(url)
+# For each Submission.id in submission_ids
+for submission in submission_ids:
+	# Plot all graphs
+	logger.info(f'Plotting submission: {submission}')
+	plot_all_graphs(dt, submission)
